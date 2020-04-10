bb2e25018601b5b942799f8122f82bb9b2d2bfef
yimae20 <yimae@minebeamitsumi.com>
2020-04-10 09:24:01 +0900

    テスト実行時にtasks.jsonを削除する処理を追加

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
+
+//テストの前に永続化されているファイルを消す
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
+    const todo = require('./index.js');
 
 // todo と list のテスト
 todo.todo('ノートを買う');
@@ -19,3 +23,4 @@ assert.deepEqual(todo.list(), []);
 assert.deepEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+});
